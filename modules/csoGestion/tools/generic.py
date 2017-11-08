2b42fb70090a90241af8c957f521c62b71f86d69
Valentin Brosseau <c4software@gmail.com>
2017-11-08 22:39:31 +0100

    Refactor

--- modules/csoGestion/tools/generic.py
+++ modules/csoGestion/tools/generic.py
@@ -1,17 +1,15 @@
-from models import Group, UserDroit, Application
+from models import UserDroit, Application
 from flask import url_for
 
 def get_tbl_object(tblName):
-	return {
-		"groups":Group,
-		"users":UserDroit,
-		"apps":Application,
-	}[tblName]
+    return {
+        "users":UserDroit,
+        "apps":Application,
+    }[tblName]
 
 def get_listing_redirection(tblName):
-	return {
-		"groups":url_for("csoGestion.list", tblName=tblName),
-		"users":url_for("csoGestion.list", tblName=tblName),
-		"apps":url_for("csoGestion.list", tblName=tblName),
-	}[tblName]
-
+    return {
+        "groups":url_for("csoGestion.list", tblName=tblName),
+        "users":url_for("csoGestion.list", tblName=tblName),
+        "apps":url_for("csoGestion.list", tblName=tblName),
+    }[tblName]
