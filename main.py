a1ca3e7466dc37ff48a7c61f3272af4096b4b058
Valentin Brosseau <c4software@gmail.com>
2017-11-12 15:21:04 +0100

    Add CSRF Token

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-from flask import Flask, render_template, request
+import uuid
+
+from flask import Flask, render_template, request, session, abort
 from modules.csoMain.views import csoMain
 from modules.csoGestion.views import csoGestion
 
 app = Flask(__name__)
-app.secret_key = 'li4Q~wNV7%RO?3=r.s|am^3>V:.cY22N%*>+%i=p/B6!1-HMv!/a8w@]Fk2n'
+app.secret_key = uuid.uuid4().hex
 
 #register des modules
 app.register_blueprint(csoMain)
 app.register_blueprint(csoGestion, url_prefix='/admin')
+
+@app.before_request
+def csrf_protect():
+    if request.method == "POST":
+        token = session.pop('_csrf_token', None)
+        if token != None and token != request.form.get('_csrf_token'):
+            abort(403)
+
+def generate_csrf_token():
+    if '_csrf_token' not in session:
+        session['_csrf_token'] = uuid.uuid4().hex
+    return session['_csrf_token']
+
+app.jinja_env.globals['csrf_token'] = generate_csrf_token
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
