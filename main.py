6640df7ec9515068f0c7c713674448e055a38359
Valentin Brosseau <c4software@gmail.com>
2017-11-12 15:23:32 +0100

    Add docstring

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import uuid
+"""
+Main file of CSO application
+"""
 
+import uuid
 from flask import Flask, render_template, request, session, abort
 from modules.csoMain.views import csoMain
 from modules.csoGestion.views import csoGestion
@@ -13,16 +16,24 @@ app.register_blueprint(csoGestion, url_prefix='/admin')
 
 @app.before_request
 def csrf_protect():
+    """
+    Test if CSRF if request is POST and if previous CSRF is present in the SESSION.
+    If its the case, test if the value is correct
+    """
     if request.method == "POST":
         token = session.pop('_csrf_token', None)
         if token != None and token != request.form.get('_csrf_token'):
             abort(403)
 
 def generate_csrf_token():
+    """
+    Function to generate a new csrf_token
+    """
     if '_csrf_token' not in session:
         session['_csrf_token'] = uuid.uuid4().hex
     return session['_csrf_token']
 
+# Inject the function into jinja available function
 app.jinja_env.globals['csrf_token'] = generate_csrf_token
 
 if __name__ == "__main__":
