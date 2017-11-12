a3ba2958477e77718b3f2f48315a9db5c1e60d13
Valentin Brosseau <c4software@gmail.com>
2017-11-12 15:10:01 +0100

    Rewrite to remove camelcase

--- modules/csoGestion/tools/generic.py
+++ modules/csoGestion/tools/generic.py
@@ -5,14 +5,20 @@ Generic Method to deal with the database
 from models import UserDroit, Application
 from flask import url_for
 
-def get_tbl_object(tblName):
+def get_tbl_object(tbl_name):
+    """
+    Return the table object according the table name.
+    """
     return {
         "users":UserDroit,
         "apps":Application,
-    }[tblName]
+    }[tbl_name]
 
-def get_listing_redirection(tblName):
+def get_listing_redirection(tbl_name):
+    """
+    Return the url for "list_data" for each table
+    """
     return {
-        "users":url_for("csoGestion.list", tblName=tblName),
-        "apps":url_for("csoGestion.list", tblName=tblName),
-    }[tblName]
+        "users":url_for("csoGestion.list_data", tbl_name=tbl_name),
+        "apps":url_for("csoGestion.list_data", tbl_name=tbl_name),
+    }[tbl_name]
--- modules/csoGestion/views.py
+++ modules/csoGestion/views.py
@@ -10,10 +10,16 @@ csoGestion = Blueprint('csoGestion', __name__, template_folder='templates')
 @csoGestion.route("/")
 @login_required
 def index():
+    """
+    Display the main page
+    """
     return render_template("index.html")
 
 @csoGestion.route("/login", methods=['GET','POST'])
 def login():
+    """
+    Do the login process
+    """
     if do_login(request.form.get('values', request.args.get('values', '')), request.form.get('signature', request.args.get('signature', ''))):
         return redirect(url_for('csoGestion.index'))
     else:
@@ -23,79 +29,96 @@ def login():
 @csoGestion.route("/logout")
 @login_required
 def logout():
+    """
+    Logout the user
+    """
     do_logout()
     return render_template('disconnect.html')
 
-#
-# List tblName
-#
-@csoGestion.route("/<tblName>/list")
+@csoGestion.route("/secret/list")
 @login_required
-def list(tblName):
+def list_secret():
+    """
+    List user with secret
+    """
+    return None
+
+@csoGestion.route("/<tbl_name>/list")
+@login_required
+def list_data(tbl_name):
+    """
+    List data in table name
+    """
     try:
-        tbl_object = get_tbl_object(tblName)
+        tbl_object = get_tbl_object(tbl_name)
         objects = tbl_object.query.all()
-        return render_template('list.html', list=objects, headers=tbl_object.header, action=tblName, key=tbl_object.primary_key)
+        return render_template('list.html', list=objects, headers=tbl_object.header, action=tbl_name, key=tbl_object.primary_key)
     except Exception as e:
         return redirect(url_for('csoGestion.index'))
 
-@csoGestion.route("/<tblName>/add")
+@csoGestion.route("/<tbl_name>/add")
 @login_required
-def add(tblName):
+def add(tbl_name):
+    """
+    Add a new entry in the tbl_name
+    """
     try:
-        tbl_object = get_tbl_object(tblName)
-        return render_template('formulaire.html', headers=tbl_object.header, action=tblName, key=tbl_object.primary_key, actionType="Ajouter")
+        tbl_object = get_tbl_object(tbl_name)
+        return render_template('formulaire.html', headers=tbl_object.header, action=tbl_name, key=tbl_object.primary_key, actionType="Ajouter")
     except Exception as e:
         return redirect(url_for('csoGestion.index'))
 
-@csoGestion.route("/<tblName>/get/<getElement>")
+@csoGestion.route("/<tbl_name>/get/<get_element>")
 @login_required
-def get(tblName, getElement):
+def get(tbl_name, get_element):
+    """
+    Get a specific element in the tbl_name
+    """
     try:
-        tbl_object = get_tbl_object(tblName)
-        current = tbl_object.query.filter(tbl_object.primary_key+" == '"+getElement+"'").first()
-        return render_template('formulaire.html', headers=tbl_object.header, action=tblName, key=tbl_object.primary_key, actionType="Modifier", object=current)
+        tbl_object = get_tbl_object(tbl_name)
+        current = tbl_object.query.filter(tbl_object.primary_key+" == '"+get_element+"'").first()
+        return render_template('formulaire.html', headers=tbl_object.header, action=tbl_name, key=tbl_object.primary_key, actionType="Modifier", object=current)
     except Exception as e:
         return redirect(url_for('csoGestion.index'))
 
-#
-# Sauvegarde ou Update
-#
-@csoGestion.route("/<tblName>/save", methods=['POST','GET'])
+
+@csoGestion.route("/<tbl_name>/save", methods=['POST','GET'])
 @login_required
-def save(tblName):
-    postValue = request.form
-    tbl_Object = get_tbl_object(tblName)()
+def save(tbl_name):
+    """
+    Save or update the Post data in the tbl_name
+    """
+    post_value = request.form
+    tbl_Object = get_tbl_object(tbl_name)()
 
     # Remplissage de l'objet
     for curHeader in tbl_Object.header:
-        if postValue[curHeader] == "":
+        if post_value[curHeader] == "":
             val = None
         else:
-            val = postValue[curHeader]
+            val = post_value[curHeader]
         setattr(tbl_Object, curHeader, val)
 
     db_session.merge(tbl_Object)
     db_session.commit()
-    return redirect(get_listing_redirection(tblName))
+    return redirect(get_listing_redirection(tbl_name))
 
-
-#
-# Confirmation Remove tblName
-#
-@csoGestion.route("/<tblName>/remove/<key>/confirm")
+@csoGestion.route("/<tbl_name>/remove/<key>/confirm")
 @login_required
-def removeConfirm(key,tblName):
-    return render_template('confirmation.html', tblName=tblName, key=key)
+def removeConfirm(key,tbl_name):
+    """
+    Ask if user confirm the deletion
+    """
+    return render_template('confirmation.html', tblName=tbl_name, key=key)
 
-#
-# Remove tblName
-#
-@csoGestion.route("/<tblName>/remove/<key>")
+@csoGestion.route("/<tbl_name>/remove/<key>")
 @login_required
-def remove(key,tblName):
-    tbl_object = get_tbl_object(tblName)
+def remove(key,tbl_name):
+    """
+    Delete the <key> element in the <tbl_name>
+    """
+    tbl_object = get_tbl_object(tbl_name)
     current = tbl_object.query.filter(tbl_object.primary_key+" == '"+key+"'").first()
     db_session.delete(current)
     db_session.commit()
-    return redirect(get_listing_redirection(tblName))
+    return redirect(get_listing_redirection(tbl_name))
