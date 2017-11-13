0ef3952c12a281fe5693051b3d42059d454040bf
Valentin Brosseau <c4software@gmail.com>
2017-11-13 23:35:53 +0100

    Update the setup script to initialize base data.

--- setup.py
+++ setup.py
@@ -1,2 +1,24 @@
-from database import init_db
+from database import init_db, db_session
+from models import UserDroit, Application
 init_db()
+
+# Create the first UserDroit
+username = raw_input("Your first admin username (should match with an existing LDAP account) : ")
+main_user = UserDroit(username=username)
+main_user.group = "admin,users"
+main_user.level = "10"
+
+db_session.merge(main_user)
+db_session.commit()
+
+admin_application = Application(nom="admin")
+admin_application.key = "T680SvwrxNtWPxB4DAT2"
+db_session.merge(admin_application)
+db_session.commit()
+
+admin_application = Application(nom="default")
+admin_application.key = "mWgBV6mKZ3nwhwpvMBxx"
+db_session.merge(admin_application)
+db_session.commit()
+
+print ("Setup Done.")
