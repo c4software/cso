d0687481d04d1ba9e8e0ab8269bac11236d8fe74
Valentin Brosseau <c4software@gmail.com>
2017-11-18 22:07:01 +0100

    Add comments

--- modules/csoNginx/views.py
+++ modules/csoNginx/views.py
@@ -1,3 +1,7 @@
+"""
+This module is used to manage the connection using the CSO with NGINX.
+"""
+
 import base64, json, hashlib
 from flask import Blueprint, redirect, request, session, abort
 from models import Application
@@ -10,6 +14,9 @@ csoNginx = Blueprint('csoNginx', __name__)
 
 @csoNginx.route('/checkAuth', methods=['POST'])
 def checkAuth():
+    """
+    Process the login data provided in post by the CSO.
+    """
     signature = request.forms.get("signature")
     values = request.forms.get("values")
     if check_and_set_login(values, signature):
@@ -19,12 +26,18 @@ def checkAuth():
 
 @csoNginx.route("/auth")
 def auth():
+    """
+    Test if user is currently connected
+    """
     if session.get("auth_username", None):
         return ""
     else:
         return abort(401)
 
 def check_and_set_login(json_values, remote_hash):
+    """
+    Test if data is matching the calculated hash.
+    """
     try:
         values = json.loads(base64.b64decode(json_values))
         values["key"] = "" #cso_app_key
@@ -38,12 +51,18 @@ def check_and_set_login(json_values, remote_hash):
         return False
 
 def set_data(values):
+    """
+    Save the currently connected user for the domain.
+    """
     session['auth_username'] = values["username"]
     session['auth_group'] = values["group"]
     session['auth_level'] = values["level"]
     return redirect("/")
 
 def expire_data():
+    """
+    Remove saved currently connected user.
+    """
     session.pop('auth_username')
     session.pop('auth_group')
     session.pop('auth_level')
