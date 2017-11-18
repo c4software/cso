23d0a65c0301bbe65d5160e3e7129e0f37a34602
Valentin Brosseau <c4software@gmail.com>
2017-11-18 21:15:08 +0100

    Set values in session, and remove data to expire the « session »

--- modules/csoNginx/views.py
+++ modules/csoNginx/views.py
@@ -24,8 +24,12 @@ def auth():
 def do_login(values, signature):
     pass
 
-def set_cookies():
-    pass
+def set_data(values):
+    session['auth_username'] = values["username"]
+    session['auth_group'] = values["group"]
+    session['auth_level'] = values["level"]
 
-def expire_cookies():
-    pass
+def expire_data():
+    session['auth_username'] = ""
+    session['auth_group'] = ""
+    session['auth_level'] = ""
