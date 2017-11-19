d381cca085f3aca629becc9d3ac4160eef61c764
Valentin Brosseau <c4software@gmail.com>
2017-11-19 11:06:52 +0100

    Add comments and lint in the standalone auth_request standalone client

--- nginx_auth_request/cso_client/client.py
+++ nginx_auth_request/cso_client/client.py
@@ -1,4 +1,11 @@
-import base64, json, hashlib, sys
+"""
+Standalone script to handle the CSO connexion and check state for NGINX auth_request process 
+"""
+
+import base64
+import json
+import hashlib
+import sys
 from bottle import route, run, template, request, response, HTTPResponse, redirect
 
 if len(sys.argv) < 2:
@@ -8,7 +15,13 @@ if len(sys.argv) < 2:
 cso_app_key = sys.argv[1]
 
 @route('/checkAuth', method='POST')
-def checkAuth():
+def check_auth():
+    """
+    Process the login data provided in post by the CSO.
+    Input POST (automatically provided by the CSO):
+        - signature
+        - values
+    """
     signature = request.forms.get("signature")
     values = request.forms.get("values")
     if do_login(values, signature):
@@ -18,17 +31,34 @@ def checkAuth():
 
 @route("/auth")
 def auth():
+    """
+    This path will be used internaly by the nginx at each requests to test if user is "authenticated". (use the nginx auth_request)
+    Return 200 if user is connected (according the session), or 401 if user need to authenticate.
+    """
     if request.get_cookie("auth_username", secret=cso_app_key):
         return HTTPResponse(status=200)
     else:
         return HTTPResponse(status=401)
 
 def do_login(json_values, remote_hash):
+    """
+    Validate the json_values and the remote hash according the secret_key.
+    """
     try:
+        # Decode the base64 and transform the json to python format
         values = json.loads(base64.b64decode(json_values))
+
+        # Insert the "secret" key
         values["key"] = cso_app_key
-        my_hash = hashlib.sha512(json.dumps(values, separators=(',',':'))).hexdigest()
+
+        # Calculate the secret hash
+        my_hash = hashlib.sha512(json.dumps(values, separators=(',', ':'))).hexdigest()
+
+        # The secret key is no longer needed, remove it from values
         values['key'] = ""
+
+        # Hash should match, if its not the case ?
+        # the data has been manipulated by someone during the authentication process
         if remote_hash == my_hash:
             set_cookie(values)
             return True
@@ -40,11 +70,17 @@ def do_login(json_values, remote_hash):
         return False
 
 def set_cookie(values):
+    """
+    Save the current auth in cookie.
+    """
     response.set_cookie('auth_username', values["username"], httponly=True, secret=cso_app_key)
     response.set_cookie('auth_group', values["group"], httponly=True, secret=cso_app_key)
     response.set_cookie('auth_level', values["level"], httponly=True, secret=cso_app_key)
 
 def expire_cookie():
+    """
+    Expire the cookie on authentication failure.
+    """
     response.set_cookie('auth_username', "", expires=0, secret=cso_app_key)
     response.set_cookie('auth_group', "", expires=0, secret=cso_app_key)
     response.set_cookie('auth_level', "", expires=0, secret=cso_app_key)
